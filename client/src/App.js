3e258eac605836367e1a549744d8ae3974918d56
Andrew Lee <andrewlee@indeed.com>
2020-02-20 17:32:00 -0600

    WIP started react components

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import StandupNavBar from "./components/StandupNavBar";
 
 function App() {
   const [hasError, setErrors] = useState(false);
@@ -20,21 +21,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-          {console.log(standupData)}
-      </header>
+      <StandupNavBar/>
     </div>
   );
 }
